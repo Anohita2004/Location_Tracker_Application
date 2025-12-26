ba44a7eae66fb5d279c4b2a74cc0b1d321dfcfbc
Anohita Mukherjee <anohitamukherjee@gmail.com>
2025-12-26 17:19:51 +0530

    feat: implement initial backend server with Express, Socket.IO, PostgreSQL, and location tracking APIs.

--- server/index.js
+++ server/index.js
@@ -199,7 +199,7 @@ const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
 // For any other request, send back index.html (SPA logic)
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
 });
 
